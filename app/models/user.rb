afa6bf8b56c98c21a65d8326b77e11de2762fdf2
Reyna DeLoge <reyna.deloge@gmail.com>
2015-05-19 14:05:13 -0600

    create user model

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ActiveRecord::Base
+  
+  def self.soundcloud_client(options={})
+    options = {
+      :client_id     => env['SOUNDCLOUD_CLIENT_ID'],
+      :client_secret => env['SOUNDCLOUD_CLIENT_SECRET'],
+    }.merge(options)
+
+    Soundcloud.new(options)
+  end
+
+
+  def soundcloud_client(options={})
+    options= {
+      :expires_at    => soundcloud_expires_at,
+      :access_token  => soundcloud_access_token,
+      :refresh_token => soundcloud_refresh_token
+    }.merge(options)
+
+    client = self.class.soundcloud_client(options)
+
+    # define a callback for successful token exchanges
+    # this will make sure that new access_tokens are persisted once an existing
+    # access_token expired and a new one was retrieved from the soundcloud api
+    client.on_exchange_token do
+      self.update_attributes!({
+        :soundcloud_access_token  => client.access_token,
+        :soundcloud_refresh_token => client.refresh_token,
+        :soundcloud_expires_at    => client.expires_at,
+      })
+    end
+
+    client
+  end
+end
