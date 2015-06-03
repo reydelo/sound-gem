9d0e5515dfd0c484100ec4bc838e196559de1483
Reyna DeLoge <reyna.deloge@gmail.com>
2015-06-03 15:49:54 -0600

    add user show page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @sc_user = current_user.soundcloud_client.get("/users/#{@user.soundcloud_user_id}")
+  end
+
+end
