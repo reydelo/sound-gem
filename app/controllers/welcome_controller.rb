3b8e4a9821dee14327078bf4c9771de899da0f3d
Reyna DeLoge <reyna.deloge@gmail.com>
2015-06-03 14:30:20 -0600

    add avatar_url attribute to users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,6 @@ class WelcomeController < ApplicationController
   end
 
   def show
-    @me = current_user.soundcloud_client.get("/me")
-    @friend = current_user.soundcloud_client
     @stream = []
     current_user.friends.map do |friend|
       friend.tracks.each do |track|
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529180027) do
+ActiveRecord::Schema.define(version: 20150603182002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150529180027) do
     t.string   "soundcloud_access_token"
     t.string   "soundcloud_refresh_token"
     t.datetime "soundcloud_expires_at"
+    t.string   "avatar_url"
   end
 
 end
